b19f0e2fdf4fae886b963955127e97cfc398c0b5
pixelyloaf <182298061+pixelyloaf@users.noreply.github.com>
2026-04-07 11:41:24 +0100

    i wonder if i can just copy and paste it

--- qrgenerator.py
+++ qrgenerator.py
@@ -2,7 +2,7 @@ import glob
 import qrcode
 
 files = glob.glob("configs/*/rehid.json")
-files = glob.glob("testconfigs/*/rehid.json")
+testfiles = glob.glob("testconfigs/*/rehid.json")
 
 i = 0
 for file in files:
@@ -12,5 +12,16 @@ for file in files:
         loc = file[file.find("/") + 1:]
         loc = loc[:loc.find("/")]
         img.save("configs/" + loc + "/qr.png")
+        f.close()
+        i = i + 1
+
+i = 0
+for file in testfiles:
+    with open(file, "r") as f:
+        data = f.read()
+        img = qrcode.make(data)
+        loc = file[file.find("/") + 1:]
+        loc = loc[:loc.find("/")]
+        img.save("testconfigs/" + loc + "/qr.png")
         f.close()
         i = i + 1
